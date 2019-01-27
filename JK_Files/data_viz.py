20e4fb1a7b87a0b18fd5a9f4286dd442dfa4fcbc
John Kim <johnary_kim@hotmail.com>
2019-01-27 00:36:11 -0500

    Adding some analytics and enabling tools

--- JK_Files/data_viz.py
+++ JK_Files/data_viz.py
@@ -22,14 +22,27 @@ If we ever want to call
 #x = tt.consolidate()
 '''
 # Use local file instead
-tt = pd.read_csv("/Users/johnkim/Documents/GitHub/CH4/JK_Files/export.csv")
+tt_df = pd.read_csv("/Users/johnkim/Documents/GitHub/CH4/JK_Files/export.csv")
 # Basic visualizations! 
 # Count State 
-state = tt["state"].value_counts()
+state = tt_df["state"].value_counts()
+# Count Style
+style = tt_df["style"].value_counts()
+# Count Artist 
+artist_name = tt_df["artistName"].value_counts()
+# Count Song Name
+song_name = tt_df["songName"].value_counts()
 
-def plot_graph(): 
-    plot = figure(x_range=list(state.index),plot_height=300,title="State Count",toolbar_location=None, tools="")
+def plot_state_count(): 
+    plot = figure(x_range=list(state.index),plot_height=300,title="State Count")
     plot.vbar(x=list(state.index), top=list(state), width=0.9)
+    plot.xgrid.grid_line_color = None
+    plot.y_range.start = 0 
+    return(plot)
+
+def plot_artist_count():
+    plot = figure(x_range=list(artist_name.index),plot_height=300,title="Artist Count",toolbar_location=None, tools="")
+    plot.vbar(x=list(artist_name.index), top=list(artist_name), width=0.9)
     plot.xgrid.grid_line_color = None
     plot.y_range.start = 0 
     return(plot)
@@ -38,9 +51,9 @@ app = Flask(__name__)
 
 @app.route('/')
 
-def hello():
+def visualize():
     
-    p = plot_graph()
+    p = plot_state_count()
     div,script = components(p)
     
     kwargs = {'plot_script': script, 'plot_div': div}
@@ -57,11 +70,11 @@ if __name__ == '__main__':
 
 '''
 # Count Style
-style = tt["style"].value_counts()
+style = tt_df["style"].value_counts()
 # Count Artist 
-artist_name = tt["artistName"].value_counts()
+artist_name = tt_df["artistName"].value_counts()
 # Count Song Name
-song_name = tt["songName"].value_counts()
+song_name = tt_df["songName"].value_counts()
 '''
 '''
 output_file("states.html")

8137013a149bd6f96c45324271f951a0e9b90196
John Kim <johnary_kim@hotmail.com>
2019-01-27 00:21:10 -0500

    FILE MOVEMENT AND UPDATES :)

--- JK_Files/data_viz.py
+++ JK_Files/data_viz.py
@@ -15,46 +15,44 @@ from bokeh.plotting import figure
 from bokeh.embed import components
 from flask import Flask, request, render_template, abort, Response
 
+#Data set 
+'''
+If we ever want to call 
+#tt = TouchTunes("2018-02-19","21:00:00","2018-02-19","22:00:00",0)
+#x = tt.consolidate()
+'''
+# Use local file instead
 tt = pd.read_csv("/Users/johnkim/Documents/GitHub/CH4/JK_Files/export.csv")
 # Basic visualizations! 
 # Count State 
 state = tt["state"].value_counts()
+
+def plot_graph(): 
+    plot = figure(x_range=list(state.index),plot_height=300,title="State Count",toolbar_location=None, tools="")
+    plot.vbar(x=list(state.index), top=list(state), width=0.9)
+    plot.xgrid.grid_line_color = None
+    plot.y_range.start = 0 
+    return(plot)
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def hello():
-    '''
-    plot = figure(x_range=list(state.index),plot_height=300,title="State Count",toolbar_location=None, tools="")
     
-    script, div = components(plot)
-    '''
-    
-    x = [1, 2, 3, 4, 5]
-    y = [6, 7, 2, 4, 5]
-    
-    p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
-    p.line(x, y, legend="Temp.", line_width=2)
-
-    div, script = components(p)    
+    p = plot_graph()
+    div,script = components(p)
     
     kwargs = {'plot_script': script, 'plot_div': div}
     
     if request.method == 'GET':
-        return render_template('main.html', **kwargs)
+        return render_template('index.html', **kwargs)
     
     abort(404)
     abort(Response('Hello'))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-'''
-If we ever want to call 
-#tt = TouchTunes("2018-02-19","21:00:00","2018-02-19","22:00:00",0)
-#x = tt.consolidate()
-'''
 
 
 '''
